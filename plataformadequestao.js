4b666001a375a66b24b6a5808cc81f418cf1d2b2
Roger <guilhermemoura403@gamil.com>
2021-10-04 20:05:30 -0300

    plataformadeestudos

---
+++ plataformadequestao.js
@@ -0,0 +1,29 @@
+function OpcaoA(){
+    let res = document.getElementById("res")
+    res.innerHTML = `A RESPOSTA SELECIONADA ESTÁ CORRETA! SIGA ASSIM.`
+    let image = document.createElement("img");
+    img.innerHTML = '<img src="https://64.media.tumblr.com/tumblr_magtg4Ygkn1rnkzyto1_r2_500.gifv">'
+}
+function OpcaoB(){
+    res.innerHTML = `Hmm, acho que não. <br> Lembre-se: as atividades humanas causam desequilibrios ambientais!`
+    let image = document.createElement("img");
+    img.innerHTML = '<img src="https://animecrazies.com.br/wp-content/uploads/2020/10/avaliar-animes.gif">'
+
+}
+function OpcaoC (){
+    res.innerHTML = `As relações comerciais nunca estiveram tão em alta na história da humanidade. Tente de novo!`
+    let image = document.createElement("img");
+    img.innerHTML = '<img src="https://64.media.tumblr.com/1feee12e9dea0d677c5cacc9c564595e/tumblr_ow763zmnd11rqe0rbo1_400.gifv">'
+}
+
+function OpcaoD(){
+    res.innerHTML = `Mudança natural... tem certeza? Pense um pouco sobre o que vem acarretando essas mudanças.`
+    let image = document.createElement("img");
+    img.innerHTML = '<img src="https://24.media.tumblr.com/30a7ac1a2cabf90c9113775309aa6bd1/tumblr_mg7og9MZPk1rek7mwo1_500.gif">'
+}
+
+function OpcaoE(){
+    res.innerHTML = `O texto diz muito mais sobre o que causa a degradação ambiental do que a sociedade em si. Tente novamente.`
+    let image = document.createElement("img");
+    img.innerHTML = '<img src="https://i0.wp.com/66.media.tumblr.com/dfba1050d1510028116d93c87cc80073/tumblr_p6ripxC4OB1rqe0rbo1_400.gif">'
+}
